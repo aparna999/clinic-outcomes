d79eb665752b7ac4dfd19fbd6e09cd8e2324aaf3
aparna999 <b.aparna.pyla@gmail.com>
2025-09-16 14:53:57 -0700

    simplified api call

--- src/app/services/clinic-outcomes.service.ts
+++ src/app/services/clinic-outcomes.service.ts
@@ -3,26 +3,13 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
-import {
-  ClinicOutcomes,
-  GMIData,
-  Period,
-  TimeInRangeData,
-} from '../models/outcomes';
+import { ClinicOutcomes, Period } from '../models/outcomes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClinicOutcomesService {
   constructor(private http: HttpClient) {}
-
-  // getTimeInRange(): Observable<TimeInRangeData[]> {
-  //   return this.http.get<TimeInRangeData[]>('/time-range-data.json');
-  // }
-
-  // getGMI(): Observable<GMIData[]> {
-  //   return this.http.get<GMIData[]>('/gmi-data.json');
-  // }
 
   getClinicOutcomes(period: Period): Observable<ClinicOutcomes> {
     // build file name dynamically
